97bb947a45840cd62cf000816e1cf7cd1a716b9c
Steven Howlett <ste778@hotmail.com>
2018-08-29 13:28:25 +1000

    add list add 'required' recognition

--- assignment1.py
+++ assignment1.py
@@ -19,12 +19,14 @@ def main():
         key = book_details.pop(0)
         information_to_book[key] =book_details
         books_loaded +=1
-    print(information_to_book)
     print("{} books loaded from {}".format(books_loaded,file_location))
 
     menu_choice= menu()
     while menu_choice != "q":
         if menu_choice == "l":
+            if list_book_details(information_to_book):
+                print('No more books left to read. Why not add a new book?')
+            menu_choice= menu()
 
 
 
@@ -36,6 +38,40 @@ def menu():
         menu_choice=input("Menu:\nL = List all books\nA = Add new book\nM = Mark a new book as completed\nQ = Quit").lower()
     return menu_choice
 
+def list_book_details(book_to_information):
+    book_names = book_to_information.keys()
+    book_names = sorted(book_names)
+    authors=[]
+    amounts_of_pages=[]
+    book_completes=[]
+    largest_author_length=0
+    largest_book_name_length=0
+
+    for book in book_names:
+        book_information=book_to_information[book]
+        authors.append(book_information[0])
+        amounts_of_pages.append(int(book_information[1]))
+        book_completes.append(book_information[2])
+        if len(book_information[0]) > largest_author_length:
+            largest_author_length=len(book_information[0])
+        if len(book)>largest_book_name_length:
+            largest_book_name_length=len(book)
+    total_unread_pages =0
+    total_unread_books =0
+    if len(book_completes) == 0:
+        return True
+    for i in range(0,len(book_names)):
+        book_complete=" "
+        if book_completes[i] == 'r':
+            book_complete="*"
+            total_unread_pages += amounts_of_pages[i]
+            total_unread_books += 1
+        print("{}{}. {:<{}}  by {:<{}} {:>4} pages".format(book_complete,i+1,book_names[i],largest_book_name_length,authors[i],largest_author_length,amounts_of_pages[i]))
+
+
+    print("{} books.".format(len(book_names)))
+    print("you need to read {} pages in {} books".format(total_unread_pages,total_unread_books))
+    return False
 
 if __name__ == '__main__':
     main()
